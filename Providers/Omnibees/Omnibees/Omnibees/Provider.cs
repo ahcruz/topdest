c6f4055cb033b0660f9461c4826bc2b1a7ff3cfb
Angel <angel.horacio.cruz@gmail.com>
2017-11-03 15:24:10 -0300

    Se agrega proyecto Omnibees

---
+++ Providers/Omnibees/Omnibees/Omnibees/Provider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreProvider.SharedClasses;
+using CoreProvider.SharedClasses.Interface;
+using CoreProvider.SharedClasses.Search;
+using CoreProvider.SharedClasses.Service;
+
+namespace Omnibees
+{
+    public class Provider : IProvider
+    {
+        public IList<HotelService> Search(SearchData data)
+        {
+            return new List<HotelService>()
+            {
+                new HotelService()
+                {
+                    BeginService = DateTime.Today,
+                    EndService = DateTime.Today.AddDays(5),
+                    Occupancy = new Occupancy() {Adults = 2},
+                    Price = 150.75,
+                    Room = new Room() { Description = "Habitacion doble"},
+                    Supplements = new List<Supplement>()
+                    {
+                        new Supplement()
+                        {
+                            Type = SupplementEnum.Included,
+                            Description = "All Inclusive",
+                            Price = 50
+                        }
+                    },
+                    ProviderId = "Este es un hotel de omnibees"
+                }
+            };
+        }
+    }
+}
