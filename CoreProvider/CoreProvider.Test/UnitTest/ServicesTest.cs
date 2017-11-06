7759f3802a922f89217006196b9b4c35e432cafd
Angel <angel.horacio.cruz@gmail.com>
2017-11-06 18:20:43 -0300

    Test - Implementacion de mocks

--- CoreProvider/CoreProvider.Test/UnitTest/ServicesTest.cs
+++ CoreProvider/CoreProvider.Test/UnitTest/ServicesTest.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using CoreProvider.Services;
-using CoreProvider.Services.Interface;
 using CoreProvider.SharedClasses;
 using CoreProvider.SharedClasses.Interface;
 using CoreProvider.SharedClasses.Search;
@@ -16,9 +15,30 @@ namespace CoreProvider.Test.UnitTest
         [Fact]
         public void Search_success_with_services()
         {
-            var moqActionTravel = new Mock<IProvider> { CallBase = true };
+            var moqActionTravel = new Mock<ActionTravel.Provider>().As<IProvider>();
+            moqActionTravel.CallBase = true;
 
-            moqActionTravel.Setup(x => x.Search(new SearchData())).Returns(new List<HotelService>()
+            moqActionTravel.Setup(x => x.Search(It.IsAny<SearchData>())).Returns(HotelServices());
+
+            var moqSearchManager = new Mock<SearchManager>(new List<IProvider>
+            {
+                moqActionTravel.Object
+            })
+            {
+                CallBase = true
+            };
+
+            Assert.Equal(1, moqSearchManager.Object.GetSearch().Count);
+
+        }
+
+        /// <summary>        
+        /// Genera una lista servicios de hoteles
+        /// </summary>
+        /// <returns></returns>
+        private static List<HotelService> HotelServices()
+        {
+            return new List<HotelService>()
             {
                 new HotelService()
                 {
@@ -46,18 +66,7 @@ namespace CoreProvider.Test.UnitTest
                         }
                     }
                 }
-            });
-
-            var moqSearchManager = new Mock<SearchManager>(new List<IProvider>()
-            {
-                moqActionTravel.Object
-            })
-            {
-                CallBase = true
             };
-
-            Assert.Equal(1, moqSearchManager.Object.GetSearch().Count);
-
         }
     }
 }
